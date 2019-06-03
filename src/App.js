3d88cb99247792a2eaf40029267217bb4e5fa26d
pablo <pa.romerorgz@gmail.com>
2019-06-03 02:12:48 -0500

    create cards with menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import theme from "./theme";
 import { MuiThemeProvider } from "@material-ui/core/styles";
+import ProjectCard from './components/ProjectCard'
 
 function App({ classes }) {
-  return (
-    <MuiThemeProvider theme={theme}>
-      <div className="App" />
-    </MuiThemeProvider>
-  );
+    return (
+        <MuiThemeProvider theme={theme}>
+            <div className="App" >
+                <ProjectCard />
+            </div>
+        </MuiThemeProvider>
+    );
 }
 export default App;
